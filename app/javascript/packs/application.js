59afb4eaba8d43594673cfdcefc8eb6dbd14cc96
aiasfina <aiasfina@gmail.com>
2017-04-21 01:58:09 +0800

    删除jquery和bootstrap.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,4 @@
 require('bootstrap/dist/css/bootstrap.css')
-window.$ = window.jQuery =require('jquery/dist/jquery.js')
-require('bootstrap/dist/js/bootstrap.js')
 
 import m from 'mithril'
 import {main as Main} from 'container'
